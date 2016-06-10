8a5765214bd7dd601e1da567a2f85cc3045dd6a9
Fernanda Carvalho <fernanda.bepid@gmail.com>
2016-06-10 16:07:18 -0300

    classe especifica para desenhar squares

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -2,6 +2,10 @@ package controller;
 
 import view.*;
 
+//Autores:
+//Fernanda de Miranda Carvalho: 1411287
+//Filipo Teixeira Negrão: 1221846
+
 public class Main 
 {
 	public static void main(String[] args) 
