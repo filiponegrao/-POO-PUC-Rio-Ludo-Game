61c5789c306e9e1413e90120ef8837a72bc4b749
Fernanda Carvalho <fernanda.bepid@gmail.com>
2016-06-10 16:08:13 -0300

    limpeza ludotable
    view

--- src/view/LudoTable.java
+++ src/view/LudoTable.java
@@ -71,116 +71,6 @@ public class LudoTable extends JPanel
 		//Inicializa as variáveis necessárias		
 		super.paintComponent(g);
 		this.g2d = (Graphics2D) g;
-//		
-//		final BasicStroke stroke = new BasicStroke(3.0f);
-//
-//		//Desenha os quadrados menores
-//		for (int i = 0; i < this.squares.length; i++) 
-//		{
-//			int x =  this.squares[i].xPosition() * this.squareDimension.width;
-//			int y = this.squares[i].yPosition() * this.squareDimension.height;
-//			
-//			Rectangle2D square = new Rectangle2D.Double(x,y, this.squareDimension.width, this.squareDimension.height);
-//			
-//			Boolean isStartSquare = false;
-//			Color startColor = Color.white;
-//			
-//			int coordinateXs[] = new int[] {0, 0, 0};
-//			int coordinateYs[] = new int[] {0, 0, 0};
-//
-//			if(squares[i].type() == SquareType.RedRoad)
-//			{
-//				this.g2d.setPaint(MyColors.myRed);
-//
-//			}
-//			else if(squares[i].type() == SquareType.RedStart)
-//			{
-//				this.g2d.setPaint(MyColors.myRed);
-//				isStartSquare = true;
-//				startColor = MyColors.myLightRed;
-//				//coordenandas para triangulo
-//				coordinateXs = new int[] { (int) ((int) square.getCenterX() + square.getWidth()/4), 
-//						(int) ((int) square.getMinX() + square.getWidth()/4), 
-//						(int) ((int) square.getMinX() + square.getWidth()/4)};
-//
-//				coordinateYs = new int[] { (int) square.getCenterY(), 
-//						(int) ((int) square.getMinY() + square.getWidth()/4), 
-//						(int) ((int) square.getMaxY() - square.getWidth()/4)};
-//			}
-//			else if(squares[i].type() == SquareType.GreenRoad)
-//			{
-//				this.g2d.setPaint(MyColors.myGreen);
-//			}
-//			else if(squares[i].type() == SquareType.GreenStart)
-//			{
-//				this.g2d.setPaint(MyColors.myGreen);
-//				isStartSquare = true;
-//				startColor = MyColors.myLightGreen;
-//				//coordenandas para triangulo
-//				coordinateXs = new int[] { (int) ((int) square.getCenterX()), 
-//						(int) ((int) square.getMinX() + square.getWidth()/4), 
-//						(int) ((int) square.getMaxX() - square.getWidth()/4)};
-//
-//				coordinateYs = new int[] { (int) ((int) square.getCenterY() + square.getWidth()/4), 
-//						(int) ((int) square.getMinY() + square.getWidth()/4), 
-//						(int) ((int) square.getMinY() + square.getWidth()/4)};
-//			}
-//			else if(squares[i].type() == SquareType.BlueRoad)
-//			{
-//				this.g2d.setPaint(MyColors.myBlue);
-//			}
-//			else if(squares[i].type() == SquareType.BlueStart)
-//			{
-//				this.g2d.setPaint(MyColors.myBlue);
-//				isStartSquare = true;
-//				startColor = MyColors.myLightBlue;
-//				//coordenandas para triangulo
-//				coordinateXs = new int[] { (int) ((int) square.getCenterX()), 
-//						(int) ((int) square.getMaxX() - square.getWidth()/4), 
-//						(int) ((int) square.getMinX() + square.getWidth()/4)};
-//
-//				coordinateYs = new int[] { (int) ((int) square.getCenterY() - square.getWidth()/4), 
-//						(int) ((int) square.getMaxY() - square.getWidth()/4), 
-//						(int) ((int) square.getMaxY() - square.getWidth()/4)};
-//			}
-//			else if(squares[i].type() == SquareType.YellowRoad)
-//			{
-//				this.g2d.setPaint(MyColors.myYellow);
-//			}
-//			else if(squares[i].type() == SquareType.YellowStart)
-//			{
-//				this.g2d.setPaint(MyColors.myYellow);
-//				isStartSquare = true;
-//				startColor = MyColors.myLightYellow;
-//				//coordenandas para triangulo
-//				coordinateXs = new int[] { (int) ((int) square.getCenterX() - square.getWidth()/4), 
-//						(int) ((int) square.getMaxX() - square.getWidth()/4), 
-//						(int) ((int) square.getMaxX() - square.getWidth()/4)};
-//
-//				coordinateYs = new int[] { (int) square.getCenterY(), 
-//						(int) ((int) square.getMinY() + square.getWidth()/4), 
-//						(int) ((int) square.getMaxY() - square.getWidth()/4)};
-//			}
-//			else if(squares[i].type() == SquareType.SafePoint)
-//			{
-//				this.g2d.setPaint(Color.black);
-//			}
-//			else
-//			{
-//				this.g2d.setPaint(Color.white);
-//			}
-//
-//			this.g2d.fill(square);
-//			this.g2d.setPaint(Color.black);
-//			this.g2d.setStroke(stroke);
-//			this.g2d.draw(square);
-//
-//			if(isStartSquare == true)
-//			{
-//				g2d.setPaint(startColor);
-//				g2d.fillPolygon(coordinateXs, coordinateYs, 3);
-//			}
-//		}
 		
 		this.drawSquares();
 		this.drawTriangles();
@@ -193,14 +83,12 @@ public class LudoTable extends JPanel
 		PinModel[] greens = LudoController.sharedInstance.getGreenPins();
 		PinModel[] yellows = LudoController.sharedInstance.getYellowPins();
 
-
 		PinView.drawPins(reds, g2d, this.squareDimension);
 		PinView.drawPins(blues, g2d, this.squareDimension);
 		PinView.drawPins(greens, g2d, this.squareDimension);
 		PinView.drawPins(yellows, g2d, this.squareDimension);
 		
-		addMouseListener(EventHandlers.getMouseEvent());
-		
+		addMouseListener(EventHandlers.getMouseEvent());		
 	}
 
 	public void drawSquares()
