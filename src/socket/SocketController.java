e810612f59dff396025ea06c3f7d3886cd84583d
Filipo Negrao <filiposilver@hotmail.com>
2016-11-14 22:08:29 -0200

    Envio de mensagem, e comeco de recebimento
    Recebimento ainda incompleto

--- src/socket/SocketController.java
+++ src/socket/SocketController.java
@@ -1,14 +1,19 @@
 package socket;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
+import com.sun.javafx.collections.MappingChange.Map;
+
 import model.*;
 
-public class SocketController
+public class SocketController implements Observer
 {
 
 	private static SocketController data = new SocketController();
@@ -20,6 +25,10 @@ public class SocketController
 	private int port;
 	
 	private Socket socket;
+	
+	private OutputStream output;
+	
+	private InputStream input;
 	
 	/***********************/
 	
@@ -43,7 +52,19 @@ public class SocketController
 		try {
 			
 			this.socket = new Socket(server, port);
+			
+			this.output = this.socket.getOutputStream();
+						
 			System.out.println("Conectado!");
+			
+			MessageHandler handler = new MessageHandler(this.socket);
+			
+			Thread thread = new Thread(handler);
+			thread.start();
+			
+			// para fins de teste
+			this.serverAuthenticate("filiponegrao");
+			this.serverSendGame();
 			
 		} catch (Exception e) {
 			System.out.println(e);
@@ -64,8 +85,56 @@ public class SocketController
 		}
 	}
 
+	public Boolean sendMessage(byte[] bytes)
+	{		
+		try {
+			
+			this.output.write(bytes, 0, bytes.length);			
+			this.output.flush();
+			
+			System.out.println("Mnesagem enviada com sucesso!");
+			return true;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public Boolean serverSendGame()
+	{
+		HashMap<String, Object> map =  new HashMap<String, Object>();
+		
+		map.put("teste1", "teste1");
+		map.put("teste2", "teste2");
+		
+		String text = map.toString() + "\n";
+		
+		this.sendMessage(text.getBytes());
+		
+		return true;
+	}
+	
+	public Boolean serverAuthenticate(String nickname)
+	{
+		HashMap<String, Object> map =  new HashMap<String, Object>();
+		
+		map.put("nickname", nickname);
+		
+		String text = map.toString()  + "\n";
+		
+		this.sendMessage(text.getBytes());
+		
+		return true;		
+	}
 
-	
-	
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+		HashMap<String, Object> map = (HashMap<String, Object>) arg;
+		
+		System.out.println(map);
+	}
 	
 }
